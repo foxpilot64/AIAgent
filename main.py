7bbf1531eb12791b59eff194e3dcdc55b1d6726e
foxpilot64 <jedioutcastbex@gmail.com>
2025-06-28 11:51:07 -0400

    Added no prompt error msg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -16,4 +17,11 @@ response = client.models.generate_content(
 print(response.text)
 print("Prompt tokens:", response.usage_metadata.prompt_token_count)
 print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+print("This script is called:", sys.argv[0])
+print("Arguments passed", sys.argv[1:])
+
+if len(sys.argv) < 2:
+    print("Error,no prompt detected... ")
+    sys.exit(1) #Exits script with error code 1
 
