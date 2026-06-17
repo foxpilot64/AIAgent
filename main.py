e49e05cc513834a7687bbdb19bf73e21af57c86d
foxpilot64 <49044647+foxpilot64@users.noreply.github.com>
2026-06-17 13:21:31 -0400

    Added token metadata tracking to Gemini API responses

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 import os
 from dotenv import load_dotenv
 import sys
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
 from google import genai
 
-client = genai.Client(api_key=api_key)
+
+#define the main function
+def main() -> None:
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    
+
+    client = genai.Client(api_key=api_key)
 
 
-from google.genai import types
-import argparse
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("user_prompt") # for the prompt
-parser.add_argument("--verbose", action ="store_true") # for --verbose flag
 
-args = parser.parse_args()
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
-]
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
 )
 
-print(response.text)
+#check tokens
+    if not response.usage_metadata:
+        raise RuntimeError("Something went wrong!")
 
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+    print("Response tokens: ", response.usage_metadata.candidates_token_count)
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
