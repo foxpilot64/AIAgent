17c54871035d1af737de736373065c264d7b1b4b
foxpilot64 <49044647+foxpilot64@users.noreply.github.com>
2026-06-17 13:59:38 -0400

    updated get_files_info and created text_get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -14,25 +14,13 @@ def get_files_info(working_directory, directory=None):
     directory_to_check_abs = os.path.abspath(os.path.join(working_directory_abs, directory_to_check))
 
 
-    if not directory_to_check_abs.startswith(working_directory_abs):
-        return(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    if os.path.commonpath([working_directory_abs, directory_to_check_abs]) != working_directory_abs:
+        return f'Error: Cannot list "{directory_to_check}" as it is outside the permitted working directory'
 
     if not os.path.isdir(directory_to_check_abs):
-        return(f'Error: "{directory}" is not a directory')
+        return(f'Error: "{directory_to_check}" is not a directory')
 
-    try:
-        items = os.listdir(directory_to_check_abs)
-        string_collection = []
-    
 
-        for item in items:
-            full_path = os.path.join(directory_to_check_abs, item)
-            size = os.path.getsize(full_path)
-            formatted_line = f"-{item}: file_size={size} bytes, is_dir={os.path.isdir(full_path)}"
-            string_collection.append(formatted_line)
-    
-        return "\n".join(string_collection)
-
-    except Exception as e:
-        return f"Error: {str(e)}"
+    return f'Success: "{directory_to_check}" is within the working directory'
+  
     
