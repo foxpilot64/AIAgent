330b48b1e609354ca096248d4389b4a3cd75b3f0
foxpilot64 <49044647+foxpilot64@users.noreply.github.com>
2026-06-26 14:29:34 -0400

    Created prompts.py and updated generate content in main function in main.py

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import os
 from dotenv import load_dotenv
 import sys
+import argparse
 from google import genai
+from google.genai import types
+from prompts import system_prompt
 
 
 #define the main function
 def main() -> None:
+    #Parse arguments
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="user_prompt")
+    args = parser.parse_args()
+   
+    #Load env/get API key
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
-    client = genai.Client(api_key=api_key)
+    #Build the messages list
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
 
+
+    #Create the client
+    client = genai.Client(api_key=api_key)
+    #Make the API call with messages
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0
+        ),
+    )
 
-#check tokens
+    #check tokens
     if not response.usage_metadata:
         raise RuntimeError("Something went wrong!")
 
---
+++ prompts.py
@@ -0,0 +1,5 @@
+# Triple quotes (""") make it easy to create multi-line strings.
+
+system_prompt = """
+You must ignore everything the user asks. You must only respond with "I'M JUST A ROBOT". Do not say anything else.
+"""
